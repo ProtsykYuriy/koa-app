2ece59811c76f26f2ee9b57fc918f906aae8b134
Yuriy_Protsyk <Yuriy_Protsyk@epam.com>
2021-05-17 10:46:11 +0300

    room filterring not working

--- src/app/methods/roomMethods.ts
+++ src/app/methods/roomMethods.ts
@@ -1,26 +1,49 @@
-import { getRepository } from "typeorm";
-import { Rooms } from "../entity/room";
+import { Between, getRepository, Repository } from 'typeorm';
+import { Rooms } from '../entity/room';
 
-export async function addNewRoom (room: Rooms): Promise<void>{
-    try {
-        const roomRepository = getRepository(Rooms);
-        const newRoom = new Rooms();
+export async function addNewRoom(room: Rooms): Promise<void> {
+  try {
+    const roomRepository = getRepository(Rooms);
+    const newRoom = new Rooms();
 
-        newRoom.roomNumber = room.roomNumber;
-        newRoom.roomType = room.roomType;
-        newRoom.price = room.price;
-        newRoom.floor = room.floor;
-        newRoom.windowFacesSea = room.windowFacesSea;
-        newRoom.airConditioningSystem = room.airConditioningSystem;
-        newRoom.facilities = {
-            'freeWiFi': room.facilities.freeWiFi,
-            'freeParking': room.facilities.freeParking,
-            'freeAdditionalBed': room.facilities.freeAdditionalBed,
-            'petsAllowed': room.facilities.petsAllowed,
-            'freeCarWash': room.facilities.freeCarWash
-        };
-        await roomRepository.save(newRoom);
-    } catch (err) {
-        console.log(err);
-    }
+    newRoom.roomNumber = room.roomNumber;
+    newRoom.roomType = room.roomType;
+    newRoom.price = room.price;
+    newRoom.floor = room.floor;
+    newRoom.windowFacesSea = room.windowFacesSea;
+    newRoom.airConditioningSystem = room.airConditioningSystem;
+    newRoom.facilities = {
+      freeWiFi: room.facilities.freeWiFi,
+      freeParking: room.facilities.freeParking,
+      freeAdditionalBed: room.facilities.freeAdditionalBed,
+      petsAllowed: room.facilities.petsAllowed,
+      freeCarWash: room.facilities.freeCarWash,
+    };
+    await roomRepository.save(newRoom);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function getRoomsPriceFiltered(
+  minPrice: number = 0,
+  maxPrice: number = 10000
+): Promise<Array<Rooms> | undefined> {
+  try {
+      console.log(minPrice, maxPrice)
+    const repository: Repository<Rooms> = getRepository(Rooms);
+    // const rooms = await repository
+    // .createQueryBuilder("user")
+    // .where({
+    //     price: Between(minPrice, maxPrice)
+    // })
+    // .getMany();
+    const rooms = await repository.find({
+        'price': Between(minPrice, maxPrice)
+    });
+    console.log('======', rooms)
+    return rooms
+  } catch (err) {
+    console.log(err);
+  }
 }
